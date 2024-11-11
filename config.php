a948470ab7855fb72d7ba4c2c94e0b4f540058dc
suko-wen <81161113+suko-wen@users.noreply.github.com>
2024-11-11 20:44:56 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,6 @@
 <?php
-/*define ("SERVERNAME","is3-dev.ict.ru.ac.za");
+define ("SERVERNAME","is3-dev.ict.ru.ac.za");
 define("USERNAME", "CodersCollective");
 define("PASSWORD","J3suV5V6" );
-define("DATABASE","CodersCollective");*/
-define ("SERVERNAME","sql203.infinityfree.com");
-define("USERNAME", "if0_37694551");
-define("PASSWORD","WSMiURe8kcuy" );
-define("DATABASE","if0_37694551_househub");
+define("DATABASE","CodersCollective");
 ?>
